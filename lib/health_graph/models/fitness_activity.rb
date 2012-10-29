aac3be6dc4ec870d0dff7445408982e6174e3992
Jupp Müller <jupp0r@gmail.com>
2012-10-29 08:48:26 +0100

    added more fields to fitness activity model

--- lib/health_graph/models/fitness_activity.rb
+++ lib/health_graph/models/fitness_activity.rb
@@ -2,7 +2,7 @@ module HealthGraph
   class FitnessActivity
     include Model
 
-    hash_attr_accessor :uri, :climb, :equipment, :type, :distance, :path, :calories, :total_calories, :activity, :total_distance, :duration, :start_time, :notes
+    hash_attr_accessor :uri, :climb, :equipment, :type, :distance, :path, :calories, :total_calories, :activity, :total_distance, :duration, :start_time, :notes, :average_heart_rate, :heart_rate, :gps_path
 
     def initialize(access_token, params)
       self.access_token = access_token
