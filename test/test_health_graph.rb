2c53a4d56210ad7ccd1430a4e5b113660adac647
Graham Siener <gsiener@pivotallabs.com>
2013-01-18 11:22:02 -0500

    Add Fitness Activity Summary to accept headers

--- test/test_health_graph.rb
+++ test/test_health_graph.rb
@@ -91,6 +91,7 @@ class TestHealthGraph < Test::Unit::TestCase
         :user => "application/vnd.com.runkeeper.User+json",
         :fitness_activity_feed => "application/vnd.com.runkeeper.FitnessActivityFeed+json",
         :fitness_activity => "application/vnd.com.runkeeper.FitnessActivity+json",
+        :fitness_activity_summary=> "application/vnd.com.runkeeper.FitnessActivitySummary+json",
         :new_fitness_activity => "application/vnd.com.runkeeper.NewFitnessActivity+json",      
         :strength_training_activity_feed => "application/vnd.com.runkeeper.StrengthTrainingActivityFeed+json",
         :strength_training_activity => "application/vnd.com.runkeeper.StrengthTrainingActivity+json",    
